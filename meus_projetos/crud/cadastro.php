77bef13e007ed8e6349efc4593f6ff8d8df6d650
luizfelipecirqueira <lfcs18ts@gmail.com>
2020-11-06 15:22:05 -0300

    Commit inicial do crud em php
    Praticando um Crud simples em php.

---
+++ meus_projetos/crud/cadastro.php
@@ -0,0 +1,36 @@
+<?php
+    $login = $_POST['login'];
+    $senha = $_POST['senha'];
+
+    $connect = mysql_connect('localhost', 'root', '');
+    $db = mysql_select_db('crud');
+    $query_select = "SELECT login from usuarios WHERE login = '$login'";
+    $select = mysql_query($query_select, $connect);
+    $array = mysql_fetch_array($select);
+    $logarray = $array['login'];
+
+    if($login == "" || $login == null){
+        echo "<script language='javascript' type='text/javascript'>
+        alert('O campo login precisa ser preenchido');window.location.href='cadastro.html';</script>";
+    }
+    else{
+        if($logarray == $login){
+            echo "<script language='javascript' type='text/javascript'>
+            alert('Esse login já existe');window.location.href='cadastro.html';</script>";
+            die();
+        }
+        else{
+            $query = "INSERT INTO usuarios (login, senha) VALUES ('$login', '$senha')";
+            $insert = mysql_query($query, $connect);
+
+            if($insert){
+                echo "<script language='javascript' type='text/javascript'>
+                alert('Usuário Cadastrado com Sucesso!');window.location.href='cadastro.html';</script>";
+            }
+            else{
+                echo"<script language='javascript' type='text/javascript'>
+                alert('Não foi possível cadastrar esse usuário');window.location.href='cadastro.html'</script>";
+            }
+        }
+    }
+?>

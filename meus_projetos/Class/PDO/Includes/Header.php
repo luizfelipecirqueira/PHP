32dcb26e9318d5ad638738a513a6523c9b7d26b5
luizfelipecirqueira <lfcs18ts@gmail.com>
2020-11-28 20:04:06 -0300

    Commit layout simples
    Realizando o layout simples para a aplicação. Modificação no index.php e criação dos arquivos Header e Footer, alem do style.css

---
+++ meus_projetos/Class/PDO/Includes/Header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Class Crud</title>
+</head>
+<body>
+    <div class="Nav">
+        <li><a href="cadastro">Cadastro</></li>
+        <li><a href="selecao">Seleção</></li>
+        <li><a href="delete">Delete</></li>
+        <li><a href="atualizacao">Atualização</></li>
+    </div>
--- meus_projetos/Class/PDO/index.php
+++ meus_projetos/Class/PDO/index.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-    <?php
-        include ("ClassConexao.php");
+<?php include("Includes/Header.php");?>
+    <div class="Content">
+        <?php
+            include ("ClassConexao.php");
         
-    ?>
-</html>
+        ?>
+    </div>
+<?php include("Includes/Footer.php");?>
